4be40e34b144e3087c26076e100ecfdebbf688fd
MAO22nd <129629549+MAO22nd@users.noreply.github.com>
2023-06-13 20:06:14 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,9 +20,9 @@ function preload(){
   
   cloudImage = loadImage("cloud.png");
   
-  obstacle1 = loadImage("obstacle1.png");
-  obstacle2 = loadImage("obstacle2.png");
-  obstacle3 = loadImage("obstacle3.png");
+  obstacle1 = loadImage("png-clipart-bone-bone-remuvebg-preview(2)png");
+  obstacle2 = loadImage("png-clipart-bone-bone-remuvebg-preview(2)png");
+  obstacle3 = loadImage("png-clipart-bone-bone-remuvebg-preview(2)png");
   obstacle4 = loadImage("obstacle4.png");
   obstacle5 = loadImage("obstacle5.png");
   obstacle6 = loadImage("obstacle6.png");
